2f3fb01de164eab5647632f1c8caa0d6fb8dd230
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-06 13:00:59 +0600

    feat(utils): Super Seeded Durng Starting the Server

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -11,10 +11,12 @@ interface EnvConfig {
     JWT_ACCESS_EXPIRES: string,
     JWT_REFRESH_SECRET: string,
     JWT_REFRESH_EXPIRES: string,
+    SUPER_ADMIN_EMAIL: string,
+    SUPER_ADMIN_PASSWORD: string
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD"];
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -31,7 +33,9 @@ const loadEnvVariables = (): EnvConfig => {
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
         JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
-        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string
+        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string
     }
 }
 
