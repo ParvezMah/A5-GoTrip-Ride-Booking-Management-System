5f66d76bb52bdb00f191c379c4ee3fea31e710a4
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-06 12:53:57 +0600

    feat(middlewares): Added checkAuth Middlewares for Protecting Routes Through Role Based Access

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -1,2 +1,50 @@
-// login
-// checkauth for Role Based Access
+import { JwtPayload } from 'jsonwebtoken';
+import { NextFunction, Request, Response } from "express";
+import AppError from '../errorHelper.ts/ApiError';
+import { verifyToken } from '../utils/jwt';
+import { envVars } from '../config/env';
+import httpStatus from 'http-status';
+import {  UserStatus } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
+
+export const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const accessToken = req.headers.authorization || req.cookies.accessToken;
+
+        if (!accessToken) {
+            throw new AppError(403, "No Token Received")
+        }
+
+        const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET) as JwtPayload
+        const isUserExist = await User.findOne({ email: verifiedToken.email })
+       
+        if (!isUserExist) {
+            throw new AppError(httpStatus.BAD_REQUEST, "User Does Not Exist")
+        }
+
+        if (!isUserExist.isVerified) {
+            throw new AppError(httpStatus.BAD_REQUEST, "User is not verified")
+        }
+
+        // if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
+        //     throw new AppError(httpStatus.BAD_REQUEST, `User Is ${isUserExist.isActive}`)
+        // }
+        if (isUserExist.status === UserStatus.BLOCKED ) {
+            throw new AppError(httpStatus.BAD_REQUEST, `User Is ${isUserExist.status}`)
+        }
+       
+        if (isUserExist.isDeleted) {
+            throw new AppError(httpStatus.BAD_REQUEST, "User Is Deleted")
+        }
+        
+        if (!authRoles.includes(verifiedToken.role)) {
+            throw new AppError(403, "You Are Not Permitted To View This Route ")
+        }
+
+        req.user = verifiedToken
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { userControllers } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { createUserZodSchema } from "./user.validation";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "./user.interface";
 
 const router = Router();
 
