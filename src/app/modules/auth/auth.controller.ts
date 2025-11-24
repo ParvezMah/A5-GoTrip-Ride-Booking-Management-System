b17d685b569f1c5a63e03428efe24ce640e93ef8
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-24 10:37:18 +0600

    feat(auth) : Implement Reset / Change Password

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -84,7 +84,7 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
 });
 
 const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const refreshToken = req.headers.authorization;
+    const refreshToken = req.cookies.refreshToken;
     console.log("refreshToken : ", refreshToken)
     if (!refreshToken) {
         throw new AppError(httpStatus.BAD_REQUEST, "No refresh token recieved from cookies")
@@ -120,8 +120,25 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
     })
 })
 
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user
+
+    await AuthServices.resetPassword(oldPassword, newPassword, decodedToken);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Password Changed Successfully",
+        data: null,
+    })
+})
+
 export const AuthControllers = {
     credentialsLogin,
     getNewAccessToken,
-    logout
+    logout,
+    resetPassword
 }
