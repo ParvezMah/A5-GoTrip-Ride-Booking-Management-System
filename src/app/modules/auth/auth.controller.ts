6906143072ba48b3eb61f47f3ef869e9bcbd8afb
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-24 10:19:06 +0600

    feat(auth) : Set the accessToken and refreshToken in Cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -61,6 +61,17 @@ import AppError from "../../errorHelper.ts/ApiError";
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
     const loginInfo = await AuthServices.credentialsLogin(req.body);
+    
+    res.cookie("accessToken", loginInfo.accessToken, {
+        httpOnly: true, // httpOnly: true This makes the cookie inaccessible to JavaScript running in the browser (it can't be read or modified by document.cookie). Purpose: Helps protect against XSS (Cross-Site Scripting) attacks.
+        secure: false, // secure: false This means the cookie will be sent over both HTTP and HTTPS connections. Purpose: In development, you often use secure: false because you may not have HTTPS locally. In production, you should set secure: true so the cookie is only sent over HTTPS, making it more secure.
+    })
+    
+    res.cookie("refreshToken", loginInfo.refreshToken, {
+        httpOnly: true, // httpOnly: true This makes the cookie inaccessible to JavaScript running in the browser (it can't be read or modified by document.cookie). Purpose: Helps protect against XSS (Cross-Site Scripting) attacks.
+        secure: false, // secure: false This means the cookie will be sent over both HTTP and HTTPS connections. Purpose: In development, you often use secure: false because you may not have HTTPS locally. In production, you should set secure: true so the cookie is only sent over HTTPS, making it more secure.
+    })
+
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
